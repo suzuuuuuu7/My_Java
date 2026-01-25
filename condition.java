33d8d65e5a645501f09a00b2743bbb2ad5896578
suzuuuuuu7 <sujalkc324@gmail.com>
2026-01-25 20:18:48 +0545

    conditon in java

---
+++ condition.java
@@ -0,0 +1,18 @@
+public class condition{
+ public static void main(String[] args) {
+    
+int age = 100;
+
+if(age>=18){
+   
+    
+    System.out.println("YOu are eligible to vote");
+    
+}
+else{
+    System.out.println("You are not eligible to volte..");
+}
+        } 
+    }
+    
+
