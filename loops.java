6280d25924ec9c87eb651e794fb814cd5622bca0
suzuuuuuu7 <sujalkc324@gmail.com>
2026-01-25 20:27:02 +0545

    Loops in java

---
+++ loops.java
@@ -0,0 +1,14 @@
+public class loops {
+    public static void main(String[] args) {
+    int i = 0;
+    for( i =0; i<=4;i++){
+        System.out.println("Hello world");
+
+    }
+    int count = 1;
+    while (count<=10){
+        System.out.println("Hello java");
+    count+=2;    }
+}
+    
+}
